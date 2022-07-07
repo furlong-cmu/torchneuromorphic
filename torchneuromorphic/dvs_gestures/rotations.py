93d125ff4ea63d01862805920220e3be7f09db1d
P. Michael Furlong <michael.furlong@uwaterloo.ca>
2022-07-07 18:40:17 -0400

    updated rotations helper file to work with the data format that is extracted in create_hdf5

--- torchneuromorphic/dvs_gestures/rotations.py
+++ torchneuromorphic/dvs_gestures/rotations.py
@@ -6,30 +6,40 @@ def make_new_classes(sample, label, num_gestures=11):
     samples = [sample]
     labels = [label]
     for d_idx, d in enumerate(degs):
+        homogeneous_samples = np.hstack((samples, 
+                                         np.ones((samples.shape[0],1)))).T
         mat = make_rot_matrix(np.deg2rad(d))
-        samples.append(mat @ sample)
+        rotated_samples = mat @ homogeneous_samples
+        # hack; 4 should not be hard coded
+        samples.append( (rotated_samples.T)[:,:4]) 
         labels.append(label + d_idx * num_gestures)
     return samples, labels
 
 
 def make_rot_matrix(angle, x_c=64, y_c=64):
-    sub_mat = np.array([[1, 0, 0, 0, -x_c],
-                       [0, 1, 0, 0, -y_c],
-                       [0, 0, 1, 0, 0], # time
+    '''
+    This function is assuming the original data is in the format
+    (time, x, y, polarity, 1) and that samples are contained in 
+    columns, not rows.
+    '''
+    sub_mat = np.array([[1, 0, 0, 0, 0], # time
+                        [0, 1, 0, 0, -x_c],
+                       [0, 0, 1, 0, -y_c],
                        [0, 0, 0, 1, 0]]) # polarity
                        [0, 0, 0, 0, 1]])
 
     rot_mat = np.array([
-        [ np.cos(angle), np.sin(angle),  0, 0, 0],
-        [ -np.sin(angle), np.cos(angle), 0, 0, 0],
-        [  0    ,               0      , 1, 0, 0], # time
+        [  1    ,               0      , 0, 0, 0], # time
+        [  0,  np.cos(angle), np.sin(angle),  0, 0],
+        [  0,   -np.sin(angle), np.cos(angle), 0, 0],
         [  0    ,               0      , 0, 1, 0], # polarity
         [  0    ,               0      , 0, 0, 1]])
                         
 
-    add_mat = np.array([[1, 0, 0, 0, x_c],
-                       [0, 1, 0, 0, y_c],
-                       [0, 0, 1, 0, 0], # time
+    add_mat = np.array([
+                       [1, 0, 0, 0, 0], # time
+                       [0, 1, 0, 0, x_c],
+                       [0, 0, 1, 0, y_c],
                        [0, 0, 0, 1, 0]]) # polarity
                        [0, 0, 0, 0, 1]])
 
