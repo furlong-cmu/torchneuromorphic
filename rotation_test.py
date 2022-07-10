66ca50a3b8c6b02d6f439338228984e6ba066904
Peng Zhou <zhoupengfun@gmail.com>
2022-07-10 17:57:31 -0600

    add rotation test file

---
+++ rotation_test.py
@@ -0,0 +1,22 @@
+from torchneuromorphic.dvs_gestures.dvsgestures_dataloaders import *
+import snntorch.spikeplot as splt
+import matplotlib.pyplot as plt
+
+train_dl, test_dl = create_dataloader(
+    root='/Users/ZP/Downloads/DVSbuild/dvs_gestures_build19.hdf5',
+    batch_size=32,
+    ds=4,
+    num_workers=0)
+
+# data = iter(train_dl)
+# data_it = next(data._sampler_iter)
+fig, ax = plt.subplots()
+
+for i, v in enumerate(train_dl):
+    print(np.argmax(v[1][0]))
+    tt = v[0][0,:,1,:,:]
+    anim = splt.animator(tt, fig, ax)
+    plt.show()
+    break
+
+pass
